ec6b652b14bf0d40343649f7836d3d0b696dc059
ibrahimbitar963 <ibrahim.bitar963@gmail.com>
2022-02-25 16:46:59 +0200

    cubit done

---
+++ lib/cubit/weather_cubit.dart
@@ -0,0 +1,24 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter_cubit_bloc_tutorial/data/model/weather.dart';
+import 'package:flutter_cubit_bloc_tutorial/data/weather_repository.dart';
+import 'package:meta/meta.dart';
+
+part 'weather_state.dart';
+
+class WeatherCubit extends Cubit<WeatherState> {
+  WeatherCubit(this._weatherRepository) : super(WeatherInitial());
+  final WeatherRepository _weatherRepository;
+  Future<void> getWeather(String cityName) async{
+
+    try{
+      emit(WeatherLoading());
+      final weather = await _weatherRepository.fetchWeather(cityName);
+      emit(WeatherLoaded(weather));
+    }
+    on NetworkException{
+      emit(WeatherError('network error!!!!!!'));
+    }
+
+  }
+
+}
